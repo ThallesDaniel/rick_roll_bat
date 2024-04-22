080958d2225aa0ac2e292372d68280ee2c7546a3
Thalles Daniel <106764514+ThallesDaniel@users.noreply.github.com>
2024-04-22 16:04:01 -0300

    Código troll

---
+++ rick.bat
@@ -0,0 +1,18 @@
+@echo off
+setlocal
+
+:: Defina o número de instâncias que você deseja abrir
+set "num_instancias=10"
+
+:: Loop para abrir o cmd e executar o comando curl
+for /l %%i in (1, 1, %num_instancias%) do (
+    start cmd /k curl ASCII.live/can-you-hear-me
+)
+
+:: Aguarde 10 minutos (600 segundos)
+timeout /t 600 /nobreak >nul
+
+:: Reproduza a música "never gonna give me up"
+powershell -c "(New-Object Media.SoundPlayer 'C:\Users\Lenovo\Downloads\Never Gonna Give You Up 16 bits.wav').PlaySync();"
+
+endlocal
